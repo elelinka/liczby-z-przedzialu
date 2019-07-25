91f0c2c8e2938872f49678c30b143893296170f9
Ewelina Gregoraszczuk <gregoraszczuk.e@gmail.com>
2019-07-25 09:24:52 +0200

    methods have been changed

--- src/com/example/controller/Numbers.java
+++ src/com/example/controller/Numbers.java
@@ -1,21 +1,25 @@
 package com.example.controller;
 
+import java.util.Locale;
+
 public class Numbers {
 
     public void showNumbersWhile() {
         double number = 0;
-        while (number <= 3.1) {
-            System.out.printf("%2.1f ", number);
-            number = number + 0.1;
+        while (number <= 3) {
+            System.out.printf(Locale.US, "%2.1f, ", number);
+            number += 0.1;
 
         }
+        System.out.printf(Locale.US, "%2.1f", number);
     }
 
     public void showNumbersDoWhile() {
         double number = 0;
         do {
-            System.out.printf("%2.1f ", number);
-            number = number + 0.1;
-        } while (number <= 3.1);
+            System.out.printf(Locale.US, "%2.1f, ", number);
+            number += 0.1;
+        } while (number <= 3);
+        System.out.printf(Locale.US, "%2.1f", number);
     }
 }
